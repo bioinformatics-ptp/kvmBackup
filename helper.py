2c82bd334d233cfe7b3699a11512ea9e0a2610b4
Paolo Cozzi <paolo.cozzi@ptp.it>
2015-10-21 10:45:20 +0200

    Working with blockcommit
    During blockcommit, I cant't pivot the image (return to the original
    update image). So, I need to do blockcommit through a system

--- helper.py
+++ helper.py
@@ -22,6 +22,9 @@ import xml.etree.ElementTree as ET
 # Logging istance
 logger = logging.getLogger(__name__)
 
+# A global connection instance
+conn = libvirt.open("qemu:///system")
+
 # una funzione che ho trovato qui: https://blog.nelhage.com/2010/02/a-very-subtle-bug/
 # e che dovrebbe gestire i segnali strani quando esco da un suprocess
 preexec_fn=lambda: signal.signal(signal.SIGPIPE, signal.SIG_DFL)
@@ -97,59 +100,140 @@ def getDisks(domain):
         raise Exception, "Targets and sources lengths are different %s:%s" %(len(sources), len(targets))
     
     #here all the devices I want to back up
-    devs = []
+    devs = {}
     
     for i in range(len(sources)):
-        devs += [(targets[i]["dev"], sources[i]["file"])]
+        devs[targets[i]["dev"]] = sources[i]["file"]
     
     #return dev, file path list
     return devs
 
-def getSnapshotXML(domain):
-    """Since I need to do a Snapshot with a XML file, I will create an XML to call
-    the appropriate libvirt method"""
+class Snapshot():
+    """A class to deal with libvirt snapshot"""
     
-    #call getDisk to get the disks to do snapshot
-    disks = getDisks(domain)
+    global conn    
     
-    #get a snapshot id
-    snapshotId = str(uuid.uuid1()).split("-")[0]
-    
-    #now construct all diskspec
-    diskspecs = []
-    
-    for disk in disks:
-        diskspecs += ["--diskspec %s,file=/var/lib/libvirt/images/%s-%s.img" %(disk[0], disk[0], snapshotId)]
-
-    my_cmd = "virsh snapshot-create-as --domain {domain_name} {snapshotId} {diskspecs} --disk-only --atomic --quiesce --print-xml".format(domain_name=domain.name(), snapshotId=snapshotId, diskspecs=" ".join(diskspecs))    
-    
-    logger.debug("Executing: %s" %(my_cmd))    
-    
-    #split the executable
-    my_cmds = shlex.split(my_cmd)
-    
-    #Launch command
-    create_xml = subprocess.Popen(my_cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=preexec_fn, shell=False)
-    
-    #read output in xml
-    snapshot_xml = create_xml.stdout.read()
-    
-    #Lancio il comando e aspetto che termini
-    status = create_xml.wait()
-    
-    if status != 0:
-        logger.error("Error for %s:%s" %(my_cmds, create_xml.stderr.read()))
-        logger.critical("{exe} return {stato} state".format(stato=status, exe=my_cmds[0]))
-        raise Exception, "snapshot-create-as didn't work properly"
+    def __init__(self, domain_name):
+        """Instantiate a SnapShot instance from a domain name"""
         
-    return snapshot_xml
+        self.domain_name = domain_name
+        self.snapshot_xml = None
+        self.disks = None
+        self.snapshot_disk = None
+        self.snapshotId = None
+        self.conn = conn
+        self.snapshot = None
+        
+    def getDomain(self):
+        """Return the libvirt domain by domain_name attribute class"""
+        
+        return self.conn.lookupByName(self.domain_name)
+        
+    def getDisks(self):
+        """Call getDisk on my instance"""
+        
+        #get my domain
+        domain = self.getDomain()
+        
+        #call getDisk to get the disks to do snapshot
+        return getDisks(domain)
+        
+    def dumpXML(self):
+        """Call dumpXML on my instance"""
+        
+        #get my domain
+        domain = self.getDomain()
+        
+        #call getDisk to get the disks to do snapshot
+        return dumpXML(domain)
 
-def callSnapshot(domain):
-    """Create a snapshot for domain"""
+    def getSnapshotXML(self):
+        """Since I need to do a Snapshot with a XML file, I will create an XML to call
+        the appropriate libvirt method"""
+        
+        #get my domain
+        domain = self.getDomain()
+        
+        #call getDisk to get the disks to do snapshot
+        self.disks = self.getDisks()
+        
+        #get a snapshot id
+        self.snapshotId = str(uuid.uuid1()).split("-")[0]
+        
+        #now construct all diskspec
+        diskspecs = []
+        
+        for disk in self.disks.iterkeys():
+            diskspecs += ["--diskspec %s,file=/var/lib/libvirt/images/snapshot_%s_%s-%s.img" %(disk, self.domain_name, disk, self.snapshotId)]
     
-    #i need a xml file for the domain
-    pass
-    
+        my_cmd = "virsh snapshot-create-as --domain {domain_name} {snapshotId} {diskspecs} --disk-only --atomic --quiesce --print-xml".format(domain_name=domain.name(), snapshotId=self.snapshotId, diskspecs=" ".join(diskspecs))    
+        
+        logger.debug("Executing: %s" %(my_cmd))    
+        
+        #split the executable
+        my_cmds = shlex.split(my_cmd)
+        
+        #Launch command
+        create_xml = subprocess.Popen(my_cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=preexec_fn, shell=False)
+        
+        #read output in xml
+        self.snapshot_xml = create_xml.stdout.read()
+        
+        #Lancio il comando e aspetto che termini
+        status = create_xml.wait()
+        
+        if status != 0:
+            logger.error("Error for %s:%s" %(my_cmds, create_xml.stderr.read()))
+            logger.critical("{exe} return {stato} state".format(stato=status, exe=my_cmds[0]))
+            raise Exception, "snapshot-create-as didn't work properly"
+            
+        return self.snapshot_xml
+
+    def callSnapshot(self):
+        """Create a snapshot for domain"""
+        
+        #Don't redo a snapshot on the same item
+        if self.snapshot is not None:
+            logger.error("A snapshot is already defined for this domain")
+            logger.warn("Returning the current snapshot")
+            return self.snapshot
+        
+        #i need a xml file for the domain
+        if self.snapshot_xml is None:
+            self.getSnapshotXML()
+            
+        #Those are the flags I need for creating SnapShot
+        [disk_only, atomic, quiesce] = [libvirt.VIR_DOMAIN_SNAPSHOT_CREATE_DISK_ONLY, libvirt.VIR_DOMAIN_SNAPSHOT_CREATE_ATOMIC, libvirt.VIR_DOMAIN_SNAPSHOT_CREATE_QUIESCE]
+        
+        #get a domain
+        domain = self.getDomain()
+        
+        #do a snapshot
+        logger.info("Creating snapshot %s for %s" %(self.snapshotId, self.domain_name))
+        self.snapshot = domain.snapshotCreateXML(self.snapshot_xml, flags=sum([disk_only, atomic, quiesce]))
+        
+        #Once i've created a snapshot, I can read disks to have snapshot image name
+        self.snapshot_disk = self.getDisks()
+        
+        return self.snapshot
+        
+    def doBlockCommit(self):
+        """Do a blockcommit for every disks shapshotted"""
+        
+        #get a domain
+        domain = self.getDomain()      
+        
+        ##Those are the flags I need for blockcommit
+        [active] = [libvirt.VIR_DOMAIN_BLOCK_COMMIT_ACTIVE]
+        
+        #A blockcommit for every disks. Using names like libvirt variables. Base is the original image file
+        for disk, base in self.disks.iteritems():
+            #Top is the image file I want to commit. In this case, is the snapshot file
+            top = self.snapshot_disk[disk]
+            
+            #virsh  blockcommit DockerNode2 hda --active --verbose --pivot
+            domain.blockCommit(disk, base, top, flags=sum([active]))
+        
     
 def backup(domain, parameters):
     """Do all the operation needed for backup"""
